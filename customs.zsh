84a34eba16359f8c0c79a4b56902a53a9536a010
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2022-11-22 23:50:10 +0000

    ✨Add lofi hip hop beats

--- customs.zsh
+++ customs.zsh
@@ -19,6 +19,9 @@ alias proj="cd $PROJDIR"
 # docs      Go to Documents
 alias docs="cd ~/Documents"
 
+# lofi      Open lofi hiphop beats
+alias lofi="open https://www.youtube.com/watch\?v\=jfKfPfyJRdk"
+
 # kop       Kill process running on port $1
 function kop(){
         lsof -i:$1 && echo \\nkilling $(lsof -t -i:$1) && lsof -t -i:$1 | xargs kill -9
