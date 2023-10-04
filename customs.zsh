746eef0af1304cf777456788733c4423d51e43ae
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2023-10-04 13:41:42 +0100

    add default branch detection

--- customs.zsh
+++ customs.zsh
@@ -25,6 +25,20 @@ alias lofi="open https://www.youtube.com/watch\?v\=jfKfPfyJRdk"
 # nuke      Clear all untracked files (interactive)
 alias nuke="git clean -xdfi"
 
+# ggmr      Git get default branch (with remote)
+alias ggdr="git branch -rl | grep 'origin/HEAD' | cut -d '>' -f 2 | tr -d ' '"
+
+# ggmr      Git get default branch
+alias ggd="git branch -rl | grep 'origin/HEAD' | cut -d '>' -f 2 | tr -d ' ' | sed 's/origin\///' "
+
+# grm      Git rebase main from origin
+unalias grm
+alias grm="git fetch && git rebase -i $(ggdr)"
+
+# gcm      Git checkout default brnach
+unalias gcm
+alias gcm="git checkout $(ggd)"
+
 # kop       Kill process running on port $1
 function kop(){
         lsof -i:$1 && echo \\nkilling $(lsof -t -i:$1) && lsof -t -i:$1 | xargs kill -9
