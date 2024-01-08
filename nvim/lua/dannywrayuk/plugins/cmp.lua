d77648898626651268af4bf6180f48f80de9416a
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-01-08 23:18:13 +0000

    change cmp ordering

--- nvim/lua/dannywrayuk/plugins/cmp.lua
+++ nvim/lua/dannywrayuk/plugins/cmp.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-field
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
@@ -13,6 +14,7 @@ return {
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local types = require("cmp.types")
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		local function get_lsp_completion_context(completion, source)
@@ -77,6 +79,24 @@ return {
 			end
 		end
 
+		local kindPriority = function(kind)
+			return ({
+				[types.lsp.CompletionItemKind.Variable] = 0,
+				[types.lsp.CompletionItemKind.Method] = 0,
+				[types.lsp.CompletionItemKind.Field] = 0,
+				[types.lsp.CompletionItemKind.Text] = 0,
+				[types.lsp.CompletionItemKind.Snippet] = 100,
+				[types.lsp.CompletionItemKind.Keyword] = 100,
+			})[kind] or 50
+		end
+		local compareKind = function(a, b)
+			local kind1 = kindPriority(a:get_kind())
+			local kind2 = kindPriority(b:get_kind())
+			if kind1 ~= kind2 then
+				return kind1 - kind2 < 0
+			end
+		end
+
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone",
@@ -96,7 +116,6 @@ return {
 				["<Left>"] = ifVisible(cmp.close),
 				["<Right>"] = ifVisible(cmp.close),
 			}),
-			---@diagnostic disable-next-line: undefined-field
 			sources = cmp.config.sources({
 				{ name = "luasnip" },
 				{ name = "nvim_lsp" },
@@ -104,6 +123,18 @@ return {
 				{ name = "buffer" },
 				{ name = "path" },
 			}),
+			sorting = {
+				comparators = {
+					cmp.config.compare.offset,
+					cmp.config.compare.exact,
+					compareKind,
+					cmp.config.compare.score,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
+				},
+			},
 			formatting = {
 				fields = { "kind", "abbr", "menu" },
 				format = function(entry, item)
