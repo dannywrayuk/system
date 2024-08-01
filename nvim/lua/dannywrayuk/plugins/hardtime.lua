1bb2258b91f3c38adcef71feb14bd6b915f875ef
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-08-01 10:59:24 +0100

    remove hardtime from dressing input

--- nvim/lua/dannywrayuk/plugins/hardtime.lua
+++ nvim/lua/dannywrayuk/plugins/hardtime.lua
@@ -2,7 +2,7 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	config = function()
-		require("hardtime").setup({})
+		require("hardtime").setup({ disabled_filetypes = { "DressingInput" } })
 		local keymap = require("dannywrayuk.util.keymap")
 		keymap.set("n", "<leader>ht", ":Hardtime toggle<CR>")
 	end,
