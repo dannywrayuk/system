8662a3da81515e847013d2f87916e65d433d2dfd
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-08-08 03:08:25 +0100

    cursor line

--- nvim/lua/dannywrayuk/core/opts.lua
+++ nvim/lua/dannywrayuk/core/opts.lua
@@ -38,6 +38,7 @@ vim.opt.showmode = false
 
 -- add column for linelength
 -- vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
 
 -- dont add comment on newlines
 vim.cmd([[autocmd BufEnter * set formatoptions-=cro]])
@@ -64,3 +65,42 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 		end
 	end,
 })
+
+-- Create a custom namespace
+-- local ns_id = vim.api.nvim_create_namespace("EveryFifthLine")
+
+-- -- Define the function to highlight every fifth line relative to the cursor position
+-- local function highlight_relative_fifth_lines()
+-- 	-- Define a highlight group for the full line
+-- 	-- vim.cmd("highlight clear Col§orColumn")
+
+-- 	-- Get the current buffer
+-- 	local bufnr = vim.api.nvim_get_current_buf()
+
+-- 	-- Clear existing extmarks in the custom namespace
+-- 	vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
+
+-- 	-- Get the current cursor position
+-- 	local cursor_line = vim.fn.line(".") - 1 -- Lua indexing starts at 0
+
+-- 	-- Get the total number of lines in the buffer
+-- 	local total_lines = vim.fn.line("$")
+
+-- 	-- Add new extmarks for every 5th line relative to the cursor position
+-- 	for i = cursor_line % 5, (total_lines-total_lines % 5) -1, 5 do
+-- 		vim.api.nvim_buf_set_extmark(bufnr, ns_id, i, 0, {
+-- 			end_line = i + 1,
+-- 			hl_group = "ColorColumn",
+-- 			hl_eol = true,
+-- 		})
+-- 	end
+-- end
+
+-- -- Create a new augroup
+-- local every_fifth_line_group = vim.api.nvim_create_augroup("EveryFifthLine", { clear = true })
+
+-- -- Create autocommands in the new augroup
+-- vim.api.nvim_create_autocmd({ "BufEnter", "CursorMoved" }, {
+-- 	group = every_fifth_line_group,
+-- 	callback = highlight_relative_fifth_lines,
+-- })
