f0aeb2a82729fc5685d75abbbc27959b4c5e0149
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-10-22 23:54:08 +0100

    add organise imports as seperate command

--- nvim/lua/dannywrayuk/plugins/conform.lua
+++ nvim/lua/dannywrayuk/plugins/conform.lua
@@ -1,36 +1,31 @@
 return {
-	"stevearc/conform.nvim",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				lua = { "stylua" },
-			},
-		})
+    "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                graphql = { "prettier" },
+                lua = { "stylua" },
+            },
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>p", function()
-			vim.lsp.buf.execute_command({
-				command = "typescript.organizeImports",
-				arguments = { vim.api.nvim_buf_get_name(0) },
-				title = "",
-			})
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>p", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file" })
+    end,
 }
