ebed7fb0f4f70c93d6e1a8115d43ca8b11438393
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-10-26 22:41:58 +0100

    move lsp keymaps

--- nvim/lua/dannywrayuk/plugins/lspconfig.lua
+++ nvim/lua/dannywrayuk/plugins/lspconfig.lua
@@ -21,7 +21,60 @@ return {
 		local lspconfig = require("lspconfig")
 		local mason_lspconfig = require("mason-lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local lspKeymaps = require("dannywrayuk.core.lsp.keymaps")
+		local keymap = require("dannywrayuk.util.keymap")
+		local keymaps = function(buffer)
+			keymap.set("n", "gd", vim.lsp.buf.definition, {
+				buffer = buffer,
+				desc = "Go to definition",
+			})
+
+			keymap.set(
+				{ "n", "v" },
+				"<leader>ca",
+				vim.lsp.buf.code_action,
+				{ buffer = buffer, desc = "See available code actions" }
+			)
+
+			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
+				buffer = buffer,
+				desc = "Smart rename",
+			})
+
+			keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
+				buffer = buffer,
+				desc = "Show line diagnostics",
+			})
+
+			keymap.set("n", "[d", vim.diagnostic.goto_prev, {
+				buffer = buffer,
+				desc = "Go to previous diagnostic",
+			})
+
+			keymap.set("n", "]d", vim.diagnostic.goto_next, {
+				buffer = buffer,
+				desc = "Go to next diagnostic",
+			})
+
+			keymap.set(
+				"n",
+				"K",
+				vim.lsp.buf.hover,
+				{ buffer = buffer, desc = "Show documentation for symbol under cursor" }
+			)
+
+			keymap.set("n", "<leader>rs", ":LspRestart<CR>", {
+				buffer = buffer,
+				desc = "Restart LSP",
+			})
+
+			keymap.set("n", "<leader>o", function()
+				vim.lsp.buf.execute_command({
+					command = "typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+					title = "",
+				})
+			end, { buffer = buffer, desc = "Organise imports" })
+		end
 
 		mason_lspconfig.setup({
 			ensure_installed = {
@@ -39,7 +92,7 @@ return {
 				local config = {
 					capabilities = cmp_nvim_lsp.default_capabilities(),
 					on_attach = function(_, buffnr)
-						lspKeymaps.on_attach(buffnr)
+						keymaps(buffnr)
 					end,
 				}
 
