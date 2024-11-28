bf41ffcb5edb873f8accc74826a87c28ae285046
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-11-28 02:39:45 +0000

    change toggle term keymap

--- nvim/lua/dannywrayuk/plugins/toggleterm.lua
+++ nvim/lua/dannywrayuk/plugins/toggleterm.lua
@@ -2,12 +2,13 @@ return {
 	"akinsho/nvim-toggleterm.lua",
 	config = function()
 		local toggleterm = require("toggleterm")
+		local keymap = require("dannywrayuk.util.keymap")
 
 		toggleterm.setup({
-			open_mapping = [[<leader>q]],
 			hide_number = true,
 			start_in_insert = true,
 			direction = "float",
 		})
+		keymap.set({ "n", "t" }, "<C-q>", "<cmd>ToggleTerm<CR>", { desc = "Toggles the ToggleTerm" })
 	end,
 }
