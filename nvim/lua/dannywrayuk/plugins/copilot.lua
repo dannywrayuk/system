ec7371ddce74ef0b2bc0781c6fd150455212c86f
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2025-03-10 00:36:11 +0000

    add copilot commands

--- nvim/lua/dannywrayuk/plugins/copilot.lua
+++ nvim/lua/dannywrayuk/plugins/copilot.lua
@@ -1,6 +1,17 @@
 return {
 	"github/copilot.vim",
 	config = function()
-		vim.keymap.set("i", "<C-d>", "<Plug>(copilot-dismiss)")
+		local option = require("dannywrayuk.util.option-keys")
+
+		vim.keymap.set("i", option.c .. "c", "<Plug>(copilot-dismiss)")
+		vim.keymap.set("i", option.c .. "e", function()
+			vim.cmd("Copilot enable")
+		end)
+		vim.keymap.set("i", option.c .. "d", function()
+			vim.cmd("Copilot disable")
+		end)
+		vim.keymap.set("i", option.c .. "n", "<Plug>(copilot-suggest)")
+		vim.keymap.set("i", option.c .. "l", "<Plug>(copilot-next)")
+		vim.keymap.set("i", option.c .. "h", "<Plug>(copilot-previous)")
 	end,
 }
