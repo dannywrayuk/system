a2c6e89df6bdbff6c4aac20801f5a2d28f6b122d
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2025-03-27 17:51:32 +0000

    simplify

--- nix/personal.nix
+++ nix/personal.nix
@@ -22,6 +22,7 @@ let
     "firefox"
     "hammerspoon"
   ];
+  brews = [ ];
   configHome = ".config";
   configLinks = [
     "hammerspoon"
@@ -29,6 +30,100 @@ let
     "terminal"
     "wezterm"
   ];
+  path = [
+    "/opt/homebrew/bin"
+    "/Users/${user}/.cargo/bin"
+    "/Users/${user}/${configHome}/terminal/commands"
+  ];
+  settings = {
+    defaults = {
+      dock = {
+        autohide = true;
+        show-recents = false;
+        persistent-apps = [
+          "/Applications/Firefox.app"
+          "/Applications/WezTerm.app"
+        ];
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        ShowPathbar = true;
+      };
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        InitialKeyRepeat = 14;
+        KeyRepeat = 1;
+      };
+      CustomUserPreferences = {
+        "org.hammerspoon.Hammerspoon" = {
+          MJConfigFile = "~/.config/hammerspoon/init.lua";
+        };
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
+  programs = { pkgs, ... }: {
+    neovim = {
+      defaultEditor = true;
+    };
+    fzf = {
+      enableZshIntegration = true;
+      defaultOptions = [ "--color 16" ];
+    };
+    git = {
+      userName = "Danny Wray";
+      userEmail = "48837241+dannywrayuk@users.noreply.github.com";
+      extraConfig = {
+        core = {
+          editor = "nvim";
+          autocrlf = "input";
+        };
+        push = {
+          autoSetupRemote = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        advice = {
+          statusHints = false;
+        };
+        credential = {
+          helper = [
+            "" # Empty string tells git to disregard any other config
+            "${pkgs.git-credential-manager}/bin/git-credential-manager"
+          ];
+        };
+      };
+    };
+    zsh = {
+      autocd = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      dotDir = ".config/zsh"; # Must be relative to ~
+      localVariables = {
+        CONFIG_DIR = "/Users/${user}/${configHome}";
+        PROJ_DIR = "$HOME/project";
+        SYS_CONFIG_DIR = "/Users/${user}/${configHome}/system";
+      };
+      history = {
+        expireDuplicatesFirst = true;
+        ignoreDups = true;
+        path = "/Users/${user}/${configHome}/zsh/zsh_history";
+      };
+      historySubstringSearch = {
+        enable = true;
+      };
+      profileExtra = "export SHELL_SESSIONS_DISABLE=1";
+      initExtra = builtins.readFile ./common/zshrc.zsh;
+      shellAliases = import ./common/shellAlias.nix // { };
+    };
+  };
 in
 { inputs, ... }: inputs.darwin.lib.darwinSystem {
   system = system;
@@ -36,9 +131,9 @@ in
     system = system;
   };
   modules = [
+    inputs.home-manager.darwinModules.home-manager
     ({ pkgs, ... }:
       {
-        programs.zsh.enable = true;
         environment = {
           shells = [ pkgs.zsh ];
           etc.hushlogin.enable = true;
@@ -58,136 +153,38 @@ in
           masApps = { };
           casks = casks;
           taps = [ ];
-          brews = [ ];
+          brews = brews;
         };
         system = {
           stateVersion = 4;
-          defaults = {
-            dock = {
-              autohide = true;
-              show-recents = false;
-              persistent-apps = [
-                "/Applications/Firefox.app"
-                "/Applications/WezTerm.app"
-              ];
-            };
-            finder = {
-              AppleShowAllExtensions = true;
-              AppleShowAllFiles = true;
-              FXDefaultSearchScope = "SCcf";
-              FXEnableExtensionChangeWarning = false;
-              ShowPathbar = true;
-            };
-            NSGlobalDomain = {
-              AppleInterfaceStyle = "Dark";
-              InitialKeyRepeat = 14;
-              KeyRepeat = 1;
-            };
-            CustomUserPreferences = {
-              "org.hammerspoon.Hammerspoon" = {
-                MJConfigFile = "~/.config/hammerspoon/init.lua";
-              };
-            };
-          };
-          keyboard = {
-            enableKeyMapping = true;
-            remapCapsLockToControl = true;
-          };
+        } // settings;
+        users.users.${user} = {
+          name = "${user}";
+          home = "/Users/${user}";
         };
-      }
-    )
-    inputs.home-manager.darwinModules.home-manager
-    {
-      users.users.${user} = {
-        name = "${user}";
-        home = "/Users/${user}";
-      };
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.users.${user} =
-        { pkgs, config, ... }:
-        {
-          home = {
-            packages = (map (name: pkgs.${name}) packages);
-            sessionPath = [
-              "/opt/homebrew/bin"
-              "/Users/${user}/.cargo/bin"
-              "/Users/${user}/${configHome}/terminal/commands"
-            ];
-            enableNixpkgsReleaseCheck = false;
-            stateVersion = "24.05";
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${user} =
+          { pkgs, config, ... }:
+          {
+            home = {
+              packages = (map (name: pkgs.${name}) packages);
+              sessionPath = path;
+              enableNixpkgsReleaseCheck = false;
+              stateVersion = "24.05";
+            };
+            xdg.configFile = builtins.listToAttrs (map
+              (key: {
+                name = key;
+                value = {
+                  source = (config.lib.file.mkOutOfStoreSymlink "/Users/${user}/${configHome}/system/${key}");
+                };
+              })
+              configLinks);
+            programs = (builtins.mapAttrs (k: v: v // { enable = true; }) (programs pkgs)) // {
+              home-manager.enable = true;
+            };
           };
-          xdg.configFile = builtins.listToAttrs (map
-            (key: {
-              name = key;
-              value = {
-                source = (config.lib.file.mkOutOfStoreSymlink "/Users/${user}/${configHome}/system/${key}");
-              };
-            })
-            configLinks);
-
-          programs.home-manager.enable = true;
-          programs.neovim = {
-            enable = true;
-            defaultEditor = true;
-          };
-          programs.fzf =
-            {
-              enable = true;
-              enableZshIntegration = true;
-              defaultOptions = [ "--color 16" ];
-            };
-          programs.git =
-            {
-              enable = true;
-              userName = "Danny Wray";
-              userEmail = "48837241+dannywrayuk@users.noreply.github.com";
-              extraConfig = {
-                core = {
-                  editor = "nvim";
-                  autocrlf = "input";
-                };
-                push = {
-                  autoSetupRemote = true;
-                };
-                pull = {
-                  rebase = true;
-                };
-                advice = {
-                  statusHints = false;
-                };
-                credential = {
-                  helper = [
-                    "" # Empty string tells git to disregard any other config
-                    "${pkgs.git-credential-manager}/bin/git-credential-manager"
-                  ];
-                };
-              };
-            };
-          programs.zsh = {
-            enable = true;
-            autocd = true;
-            enableCompletion = true;
-            autosuggestion.enable = true;
-            dotDir = ".config/zsh"; # Must be relative to ~
-            localVariables = {
-              CONFIG_DIR = "/Users/${user}/${configHome}";
-              PROJ_DIR = "$HOME/project";
-              SYS_CONFIG_DIR = "/Users/${user}/${configHome}/system";
-            };
-            history = {
-              expireDuplicatesFirst = true;
-              ignoreDups = true;
-              path = "/Users/${user}/${configHome}/zsh/zsh_history";
-            };
-            historySubstringSearch = {
-              enable = true;
-            };
-            profileExtra = "export SHELL_SESSIONS_DISABLE=1";
-            initExtra = builtins.readFile ./common/zshrc.zsh;
-            shellAliases = import ./common/shellAlias.nix // { };
-          };
-        };
-    }
+      })
   ];
 }
