518fe3e19e5745bc62f5f380f202c17f910eda57
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2025-03-28 13:24:14 +0000

    name

--- nix/flake.nix
+++ nix/flake.nix
@@ -17,7 +17,7 @@
     {
       darwinConfigurations = {
         DannysMacbookAir = (import ./personal.nix) { inherit inputs; };
-        Dannys-Macbook-Pro = (import ./work.nix) { inherit inputs; };
+        Dannys-MacBook-Pro = (import ./work.nix) { inherit inputs; };
       };
     };
 }
